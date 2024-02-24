5ed95524691a0eeb49d5aead8367acd47f503302
Rohit <rohit.verma@itsrizzoli.it>
2024-02-24 16:52:15 +0100

    Initial commit

---
+++ src/black_jack.js
@@ -0,0 +1,43 @@
+let sum = 0
+let pc_num = Math.floor(Math.random() * 21) + 1
+
+function draw() {
+    let number = Math.floor(Math.random() * 21) + 1
+    sum += number
+    document.getElementById("current_card").textContent = "Card: " + number
+    document.getElementById("total_card").textContent = "Sum: " + sum
+    if (sum <= 20) {
+        document.getElementById("alert").setAttribute("class", "alert alert-warning");
+        document.getElementById("alert").innerHTML = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        document.getElementById("alert").setAttribute("class", "alert alert-success");
+        document.getElementById("alert").innerHTML = "Wohoo! You've got Blackjack! 🥳"
+        document.getElementById("draw").disabled = true
+    } else {
+        document.getElementById("alert").setAttribute("class", "alert alert-danger");
+        document.getElementById("alert").innerHTML = "You've lost, Computer got " + pc_num
+        document.getElementById("draw").disabled = true
+        document.getElementById("stop").disabled = true
+    }
+    console.log(number)
+}
+
+
+function stop() {
+    document.getElementById("draw").disabled = true
+    if (sum <= 21 && sum > pc_num) {
+        document.getElementById("alert").setAttribute("class", "alert alert-success");
+        document.getElementById("alert").innerHTML = "Wohoo! You've got Blackjack! 🥳, Computer got " + pc_num
+    }else if (sum <= 21 && sum === pc_num){
+        document.getElementById("alert").setAttribute("class", "alert alert-warning");
+        document.getElementById("alert").innerHTML = "Its a draw\n" + "Computer got " + pc_num
+    }else {
+        document.getElementById("alert").setAttribute("class", "alert alert-danger");
+        document.getElementById("alert").innerHTML = "You've lost, Computer got " + pc_num
+    }
+}
+
+function refresh(){
+    window.location.reload("Refresh")
+}
+
